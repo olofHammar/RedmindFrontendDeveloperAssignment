eb708bbea9eb40eb645ebaa64be03f330a854057
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-12-14 12:56:55 +0100

    added searchbar and search functionality

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/profile.css";
 import { loadImage } from "../imageLoader";
 import { Paper } from "@material-ui/core";
 
-function Profile({
-  name,
-  imageIndex,
-}) {
+function Profile({ name, imageIndex, searchTerm }) {
+
+  useEffect(() => {
+    console.log(searchTerm)
+  }, [])
+
   return (
     <Paper className="profile-container">
-       <img className="profile-image" src={loadImage(imageIndex)}/>
-       <h2 className="title-container">{name}</h2>
+      <img className="profile-image" src={loadImage(imageIndex, searchTerm)} />
+      <h2 className="title-container">{name}</h2>
     </Paper>
   );
 }
